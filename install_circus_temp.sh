dbc427b4aaf19b963602bd1f933b61671b957a72
Sebastian Schlicht <sebschlicht@uni-koblenz.de>
2015-01-03 16:34:48 +0100

    Neo4j/Titan start/stop script dummy added

--- install_circus_temp.sh
+++
@@ -1,5 +0,0 @@
-#/bin/bash
-DIR_CRR=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
-virtualenv /tmp/circus
-/tmp/circus/bin/pip install circus
-ln -s ${DIR_CRR}/src/main/python/CommandConfigure.py /tmp/circus/lib/python2.7/site-packages/circus/commands/configure.py
