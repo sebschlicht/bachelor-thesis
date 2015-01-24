643cabab27d80d3a780b6e447bc194f89e16ba02
Sebastian Schlicht <sebschlicht@uni-koblenz.de>
2015-01-24 13:50:20 +0100

    start Circus if not running, restart if running

--- src/main/resources/restart.sh
+++ src/main/resources/restart.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 p=`pgrep circusd`
-kill -15 ${p}
-while ps -p ${p} 1>/dev/null; do sleep 1; done;
+if [ ${p} ]; then
+  kill -15 ${p}
+  while ps -p ${p} 1>/dev/null; do sleep 1; done;
+fi
 ~/circus/start.sh
 
